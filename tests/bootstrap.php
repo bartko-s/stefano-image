63b0e9a7c388f49d0b212b5a68ba3ce681e7c7d1
Bartko Stefan <bartko.stefan@gmail.com>
2017-03-31 18:57:51 +0200

    Test fail - Mockery does not support PHPUnit v6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-set_include_path(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-
 if(!defined('TEMP_BASE_DIRECOTORY')) {
     define('TEMP_BASE_DIRECOTORY', __DIR__ . '/temp');
 }
 
-include_once 'vendor/autoload.php';
-include_once 'testConfig.php';
+include_once __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ . '/testConfig.php';
